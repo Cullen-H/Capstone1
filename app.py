47bdc8a74658bc5567bc122e43ede8225f04a663
Cullen-H <cullen.hutchison@gmail.com>
2021-06-28 16:28:58 -0500

    Added uri for local testing

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ db_uri = os.environ.get('DATABASE_URL')
 if db_uri:
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri.replace('postgres://', 'postgresql://', 1)
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///food-recommender"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
